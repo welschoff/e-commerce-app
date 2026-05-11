a4e223a6793912487ba007ba389ee560d6fd5547
Hendrik <h.welschoff@gmx.de>
2026-05-11 17:11:04 +0200

    Change Home Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <main className="flex justify-center">{children}</main>
+        <main>{children}</main>
       </body>
     </html>
   );
--- components/productCard/ProductCard.tsx
+++ components/productCard/ProductCard.tsx
@@ -11,11 +11,19 @@ export type ProductCardProps = {
 
 function ProductCard({ ...props }: ProductCardProps) {
   return (
-    <div className="flex flex-col gap-2 p-2 rounded-xl w-70 items-center border border-gray-200 bg-amber-50">
-      <Image alt="Product-Image" src={props.image} width={200} height={200} />
+    <div className="flex flex-col gap-2 p-2 rounded-xl w-50 items-center border border-gray-200 bg-stone-100">
+      <div className="relative w-full h-70">
+        <Image
+          className="bg-white p-5 object-contain rounded-lg"
+          alt="Product-Image"
+          src={props.image}
+          fill
+        />
+      </div>
+
       <div className="flex flex-col gap-2">
         <span className="font-bold">{props.title}</span>
-        <span>{props.price}$</span>
+        <span>{props.price} $</span>
       </div>
     </div>
   );
