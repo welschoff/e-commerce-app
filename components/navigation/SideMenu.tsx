262e1072ff57ce100cdd07b836f20105a3c1fe90
Hendrik <h.welschoff@gmx.de>
2026-05-11 17:12:43 +0200

    Add z-index to SideMenu

--- components/navigation/SideMenu.tsx
+++ components/navigation/SideMenu.tsx
@@ -14,14 +14,14 @@ export default function SideMenu({ open, setOpen }: Props) {
       <div
         onClick={() => setOpen(false)}
         className={`
-          fixed inset-0 bg-black/40 transition-opacity
+          fixed z-10 inset-0 bg-black/40 transition-opacity
           ${open ? 'opacity-100' : 'opacity-0 pointer-events-none'}
         `}
       />
 
       <div
         className={`
-          fixed top-0 left-0 h-full w-64 bg-white shadow-lg
+          fixed z-20 top-0 left-0 h-full w-64 bg-white shadow-lg
           transform transition-transform duration-300
           ${open ? 'translate-x-0' : '-translate-x-full'}
         `}
